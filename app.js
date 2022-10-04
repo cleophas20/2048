122a9588a5cd72aa12ea9b1eca3ba2199df7aa75
Cleophas <cleophasfoy@gmail.com>
2022-10-04 11:49:33 -0500

    added beginnings of gameboard

---
+++ app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const gridDisplay = document.querySelector('.grid')
+  const scoreDisplay = document.getElementById('score')
+  const resultDisplay = document.getElementById('result')
+  const width = 4
+
+  //create a playing board
+  function createBoard() {
+    for (let i = 0; i < width*width; i++)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+})
